5b3eaa16d5fddb74e40e5fef058182e16dc26eeb
xia250808 <xia250808@139.com>
2025-09-20 14:27:07 +0800

    ...

--- CGM/tests/mat/mat4_test.cpp
+++ CGM/tests/mat/mat4_test.cpp
@@ -11,9 +11,8 @@ void test_matrix_multiplication() {
     array<float, 4> matrix4x4Arow3 = { 3.0f,5.0f,-1.0f,2.0f };
     array<float, 4> matrix4x4Arow4 = { 8.0f,6.0f,4.0f,9.0f };
     Mat4 mat1(matrix4x4Arow1, matrix4x4Arow2, matrix4x4Arow3, matrix4x4Arow4);
-    mat1.print("4x4");
     std::cout << "=== 矩阵 A ===" << std::endl;
-    std::cout << "GLM 矩阵数据:\n";
+    mat1.print("4x4");
 }
 
 // 手动添加 main 函数（程序入口）
@@ -21,5 +20,5 @@ int main() {
     std::cout << "开始运行测试..." << std::endl;
     test_matrix_multiplication();  // 调用测试函数
     std::cout << "测试结束。" << std::endl;
-    return 0;  // 返回 0 表示程序正常退出
+    return 0;  // 返回 0 表示程序正常退出9
 }
