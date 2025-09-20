af3c9deeb227fb1e9c76b0bd85b13c9c54a8ca4b
xia250808 <xia250808@139.com>
2025-09-20 17:34:47 +0800

    ...

---
+++ CGM/tests/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <cassert>
+#include <iostream>
+#include "mat_test/mat4_test.h"
+
+// 手动添加 main 函数（程序入口）
+int main() {
+    std::cout << "开始运行测试..." << std::endl;
+    mat4_test::test_mat4_multiplication();
+    std::cout << "测试结束。" << std::endl;
+    return 0;  // 返回 0 表示程序正常退出9
+}
---
+++ CGM/tests/mat_test/mat4_test.h
@@ -0,0 +1,20 @@
+﻿// tests/matrix4x4_test.cpp
+#include <cassert>
+#include <iostream>
+#include<array>
+#include "mat/mat4.h"
+
+
+class mat4_test
+{
+public:
+    static void test_mat4_multiplication() {
+        array<float, 4> matrix4x4Arow1 = { 1.0f,5.0f,4.0f,2.0f };
+        array<float, 4> matrix4x4Arow2 = { 7.0f,4.0f,7.0f,2.0f };
+        array<float, 4> matrix4x4Arow3 = { 3.0f,5.0f,-1.0f,2.0f };
+        array<float, 4> matrix4x4Arow4 = { 8.0f,6.0f,5.0f,9.0f };
+        Mat4 mat1(matrix4x4Arow1, matrix4x4Arow2, matrix4x4Arow3, matrix4x4Arow4);
+        std::cout << "=== 矩阵 A ===" << std::endl;
+        mat1.print("4x4");
+    }
+};
