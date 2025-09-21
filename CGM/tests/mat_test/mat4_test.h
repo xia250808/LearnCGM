02d99bc05c19f92437aa36505960127968c76b87
xia250808 <xia250808@139.com>
2025-09-21 17:02:15 +0800

    .

--- CGM/tests/mat_test/mat4_test.h
+++ CGM/tests/mat_test/mat4_test.h
@@ -13,8 +13,8 @@ public:
         array<float, 4> matrix4x4Arow2 = { 7.0f,4.0f,7.0f,2.0f };
         array<float, 4> matrix4x4Arow3 = { 3.0f,5.0f,-1.0f,2.0f };
         array<float, 4> matrix4x4Arow4 = { 8.0f,6.0f,5.0f,9.0f };
-        Mat4 mat1(matrix4x4Arow1, matrix4x4Arow2, matrix4x4Arow3, matrix4x4Arow4);
+        Mat4 matA(matrix4x4Arow1, matrix4x4Arow2, matrix4x4Arow3, matrix4x4Arow4);
         std::cout << "=== 矩阵 A ===" << std::endl;
-        mat1.print("4x4");
+        matA.print("4x4");
     }
 };
